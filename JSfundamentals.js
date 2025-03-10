46f027b8a3ed8a85b93424d273b84dcf8fc77edd
Coralis <coralis.jpg@gmail.com>
2025-03-09 22:02:34 -0400

    commit

---
+++ JSfundamentals.js
@@ -0,0 +1,29 @@
+// creating a script that gathers data, processes it, and then outputs a consistent result.
+// 4 different types of data : 
+// CourseInfo|AssignmentGroup|AssignmentInfo|LearnerSubmission
+
+// let CourseInfo = ('a = ${a}, b = ${b}')
+
+
+// console.log = ('a = ${a}, b = ${b}')
+
+
+
+let learnears = {
+     id: 125
+     score1: 47,
+     points_posibble1:50,
+     score2:150
+     points_posibble2:150
+
+     function weightedAverage(learnears){
+          let score1 = learnear score1
+          let points_posibble1 = learnears points_posibble1
+          let score1= learners score1
+          let points_posibble1 = learnears points_posibble1
+
+          let average =(score1+ score2) / (points_posibble1 + points_posibble1);
+          return average;
+     }
+} 
+console.log (weightedAverage(learnears));
